adb62f5a026cd86ca02248de002457399c37231e
Ikeda-Hinata <ec2-user@ip-172-31-95-71.ec2.internal>
2024-07-16 05:38:10 +0900

    blog_show_condition

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
-<html lang = "ja">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset = "UTF-8">
-        <title>blog</title>
-         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <h1>Blog Name</h1>
-        <div class = "posts">
-            <div class = "post">
-                <h2 class = "title">Title</h2>
-                <p class = "body">This is a sample body.</p>
-            </div>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
